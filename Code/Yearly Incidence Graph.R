cbab8e79a10d1d5ad2fb9929281ff247a6fa84b8
r-weber <33103964+r-weber@users.noreply.github.com>
2020-04-29 16:07:02 -0600

    Add files via upload

--- Code/Yearly Incidence Graph.R
+++ Code/Yearly Incidence Graph.R
@@ -15,31 +15,19 @@ library(MASS)
 options(scipen=999)
 
 # read in data
-shoot <- read.csv(file = "C:/Users/rache/Documents/Capstone/Data/Historic Data 2006_2019.csv", 
-                  sep = ",", na.strings=c("","NA"))
+shoot <- read.csv(file = "C:/Users/rache/Documents/Capstone/Data/NYC_extracolumns_0619.csv")
 
-shoot$PRECINCT <- as.factor(shoot$PRECINCT)
 shoot$OCCUR_DATE <- anytime(shoot$OCCUR_DATE)
-shoot$month <- as.numeric(format(shoot$OCCUR_DATE, "%m"))
-shoot$day <- as.numeric(format(shoot$OCCUR_DATE, "%d"))
-shoot %<>% 
-  mutate(year = lubridate::year(OCCUR_DATE))
 
-all_dates <- shoot %>% 
-  mutate(OCCUR_DATE = as.Date(OCCUR_DATE)) %>% 
-  complete(OCCUR_DATE = seq.Date(min(OCCUR_DATE), max(OCCUR_DATE), by = "day"))
+shoot$year <- as.factor(shoot$year)
+
+colourCount <- length(unique(shoot$year)) 
+coul <- brewer.pal(4, "RdYlBu")
+
+g1 <- ggplot(shoot, aes(x = factor(year), fill = year)) + geom_bar() + 
+  geom_text(stat='count', aes(label=..count..), vjust = -1) +
+  ggtitle("Number of Shootings per Year") + theme_minimal() +
+  xlab("Year") + ylab("Count") + ylim(0,2500) + guides(fill=FALSE) +
+  scale_fill_manual(values = colorRampPalette(coul)(colourCount))
 
 
-all_dates$month <- as.numeric(format(all_dates$OCCUR_DATE, "%m"))
-all_dates$day <- as.numeric(format(all_dates$OCCUR_DATE, "%d"))
-all_dates %<>% 
-  mutate(year = lubridate::year(OCCUR_DATE))
-
-sum_shoot_year <- all_dates %>% 
-  group_by(year) %>%
-  summarise(number = n()) 
-
-ggplot(all_dates, aes(x = as.factor(year))) + geom_bar() + 
-  geom_text(stat='count', aes(label=..count..), vjust=-1) +
-  ggtitle("Number of Shootings by Year") + theme_minimal() +
-  xlab("Year") + ylab("N shootings") + ylim(0,2500)
